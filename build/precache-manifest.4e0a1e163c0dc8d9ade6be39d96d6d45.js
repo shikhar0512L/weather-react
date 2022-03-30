f1661418edb5dd3e2e27daa4506f5ff3c5517c79
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2022-03-30 22:59:08 +0530

    Add files via upload

---
+++ build/precache-manifest.4e0a1e163c0dc8d9ade6be39d96d6d45.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6f9c7d878374bb90aa2b72aeb7a623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa4e697f34db1e315064",
+    "url": "/static/css/2.de3d5785.chunk.css"
+  },
+  {
+    "revision": "5b7d4e4e206b66b7eac3",
+    "url": "/static/css/main.c0d2a24b.chunk.css"
+  },
+  {
+    "revision": "fa4e697f34db1e315064",
+    "url": "/static/js/2.9b0d9541.chunk.js"
+  },
+  {
+    "revision": "5b7d4e4e206b66b7eac3",
+    "url": "/static/js/main.63abb5ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55d5567d5080cf9a8f294bad872ffc08",
+    "url": "/static/media/blur-background-1187974.55d5567d.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
